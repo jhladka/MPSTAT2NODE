17bfbb6567234a8bad8f31110d39c8d06839a738
jhladka <jarka.repakova@centrum.cz>
2017-02-06 15:18:01 +0100

    Add copyright

--- mpstat2node.py
+++ mpstat2node.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python2
+# -*- coding: utf-8 -*-
 
 """
 Read mpstat.log file and convert it by aggregating values across NUMA nodes.
 
 Read aggregation of CPUs from <lscpu> file.
 
-LICENSE:
+
+Copyright 2017, Jarmila Hladká
+
+License:
 This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from sys import stdin, stdout, stderr, exit
